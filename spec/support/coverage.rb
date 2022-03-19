b7d350e2e773137c489f5ee1639ccd45395927ce
Washington Botelho <wbotelhos@gmail.com>
2022-03-18 22:55:53 -0300

    ft: adds codecov support

---
+++ spec/support/coverage.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'codecov'
+
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+  SimpleCov.minimum_coverage(ENV.fetch('MINIMUM_COVERAGE', 80).to_i)
+
+  SimpleCov.start('rails') do
+    add_filter [
+      '/lib/generators',
+      '/vendor',
+      '/lib/normalizy/version.rb',
+    ]
+  end
+end
