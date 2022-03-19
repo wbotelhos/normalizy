16a56846a6d0542e032fdcb394565842e9a5e7f2
Washington Botelho <wbotelhos@gmail.com>
2022-03-18 22:33:59 -0300

    lint

--- spec/support/models/model_date.rb
+++ spec/support/models/model_date.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ModelDate < ActiveRecord::Base
-  normalizy :date ,           with: :date
+  normalizy :date,            with: :date
   normalizy :date_format,     with: { date: { format: '%y/%m/%d' } }
   normalizy :date_time_begin, with: { date: { adjust: :begin } }
   normalizy :date_time_end,   with: { date: { adjust: :end } }
