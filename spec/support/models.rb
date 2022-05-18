4af0e61cb60579895cc046ab5ed8e6be802d5f24
Washington Botelho <wbotelhos@gmail.com>
2022-05-18 00:23:33 -0300

    spec: extract support file

---
+++ spec/support/models.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'support/models/alias'
+require 'support/models/match'
+require 'support/models/model'
+require 'support/models/model_date'
+require 'support/models/model_money'
+require 'support/models/model_number'
+require 'support/models/model_percent'
+require 'support/models/model_slug'
+require 'support/models/model_strip'
+require 'support/models/rule'
